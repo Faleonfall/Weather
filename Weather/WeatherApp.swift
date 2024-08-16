a1b6e4ad7b20f8ef75bec08efb276a87d876cd5b
Volodymyr Kryvytskyi <VolodymyrKryvytskyi@my.uopeople.edu>
2024-08-16 07:20:44 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Volodymyr Kryvytskyi on 16.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
